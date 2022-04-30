cad6dfd9a865e9a9fb574bfdfcc0c402dd8a10eb
Satria356 <102003881+Satria356@users.noreply.github.com>
2022-04-30 12:01:40 +0700

    Update config.js

--- config.js
+++ config.js
@@ -16,11 +16,11 @@ global.APIKeys = {
 }
 
 // Other
-global.owner = ['6281252848955']
+global.owner = ['6283811034750']
 global.premium = ['0']
-global.botname = 'Zets-MD'
-global.packname = '© Zets-MD'
-global.author = '@naando.jpeg\n@alya.xzy'
+global.botname = 'Far-md'
+global.packname = '© Far-MD'
+global.author = '@fahri'
 global.sessionName = 'session'
 global.prefa = ['','!','.','🐦','🐤','🗿']
 global.sp = '⭔'
